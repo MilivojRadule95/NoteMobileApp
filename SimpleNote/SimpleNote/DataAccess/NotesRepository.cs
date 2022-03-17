8f9933361c4804f1f088ae33f450f2dc09ec681e
DESKTOP-D84HH4L\Milivoj <milivoj.raodnic@gmail.com>
2022-03-17 20:13:28 +0100

    Model logic

---
+++ SimpleNote/SimpleNote/DataAccess/NotesRepository.cs
@@ -0,0 +1,28 @@
+﻿using SimpleNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleNote.DataAccess
+{
+    internal class NotesRepository
+    {
+        private List<Note> _notes = new List<Note>();
+
+        public void AddNote(Note note)
+        {
+            _notes.Add(note);
+        }
+
+        public void DeleteNote(Guid id)
+        {
+            _notes = _notes.Where(note => note.Id != id).ToList();
+        }
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            return new List<Note>();
+        }
+    }
+}
---
+++ SimpleNote/SimpleNote/Models/Note.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleNote.Models
+{
+    internal class Note
+    {
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public Guid Id { get; }
+
+        public Note(string title, string description)
+        {
+            Title = title;
+            Description = description;
+
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new InvalidOperationException(nameof(title));
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new InvalidOperationException(nameof(description));
+            }
+
+
+        }
+
+    }
+}
